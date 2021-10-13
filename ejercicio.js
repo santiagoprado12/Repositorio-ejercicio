9082d10388f9630a7b968042601d5d751bc34934
Santiago <santiagoprado12@gmail.com>
2021-10-12 21:33:37 -0500

    objetos

---
+++ ejercicio.js
@@ -0,0 +1,44 @@
+var cliente= {
+    ID:"123456",
+    nombre:"Santiago",
+    apellido:"Prado",
+    Celular:"3122433",
+}
+var mesas={
+    num_mesa:"4",
+    cant_puestos:"3",
+}
+var ventas_facturacion={
+    Id_factura:"121321",
+    ID_pedido:"1232111",
+    cantidad_productos:"3",
+    producto:"pezcado",
+    precio_unitario:"5000",
+    precio_total:"15000",
+    nombre_cliente:"juan",
+    
+}
+var pedidos={
+    Num_pedido:"342323",
+    producto:"arroz",
+    num_mesa:"3",
+    cantidad_productos:"2",
+    estado:"pendiente/listo",
+    Nombre_mesero:"pepe",
+}
+var gastos={
+    producto:"papa",
+    precio:"2000",
+    cantidad:"5",
+    total:"10000",
+    Estado_pago:"pendiente/pago",
+}
+var rol={
+    nombre:"Administrador",
+    descripcion:"Tiene todos los permisos",
+}
+var usuarios={
+    nombre:"Juan",
+    rol:"Administrador",
+    cargo:"Gerente",
+}
